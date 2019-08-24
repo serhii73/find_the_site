b34a4ab7c6b678cade2f76587bd11ad18dd893b1
serhii <aserhii@protonmail.com>
2019-08-24 17:23:43 +0300

    :tada:

---
+++ README.md
@@ -0,0 +1,16 @@
+# find_the_site
+## Do you need a company website? This small library finds websites in duckduckgo website.
+##### Install
+
+```
+pip install find_the_site
+```
+
+##### Find the website
+
+```
+In [1]: from find_the_site.fw import get_website                                               
+
+In [2]: get_website("UNITED NATIONS")                                                          
+Out[2]: 'https://www.un.org/en/'
+```
---
+++ find_the_site/fw.py
@@ -0,0 +1,23 @@
+"""Get a website from http://ddg.gg/ ."""
+import requests
+from bs4 import BeautifulSoup
+from user_agent import generate_user_agent
+
+
+def get_website(need_website=None):
+    """Return a website."""
+    website = None
+    if need_website:
+        # Find website
+        find_website_ddg = "website " + need_website
+        ua = generate_user_agent()
+        headers = {"User-Agent": ua}
+        payload = {"q": find_website_ddg, "kl": "us-en"}
+        r = requests.post("https://duckduckgo.com/lite/", headers=headers, data=payload)
+        soup = BeautifulSoup(r.text, "html.parser")
+        website_list = [
+            i.get("href") for i in soup.find_all("a", {"class", "result-link"})
+        ]
+        if website_list:
+            website = website_list[0]
+    return website
